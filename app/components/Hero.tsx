f6a05d5595c2d78ae0f5f2b8e34887a3bfd02f8b
Andrei Malets <coolviktori@yandex.by>
2024-03-21 23:04:26 +0300

    changes About

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Hero = () => {
-  return <section className="h-[80vh] lg:h-[912px] bg-gray-400">Hero</section>;
+  return (
+    <section className="h-[80vh] lg:h-[912px] bg-gray-400" id="home">
+      Hero
+    </section>
+  );
 };
 
 export default Hero;
