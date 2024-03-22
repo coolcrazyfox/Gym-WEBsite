bf120a732c3d48989585c6c463785fa825e94fd5
Andrei Malets <coolviktori@yandex.by>
2024-03-22 16:30:17 +0300

    changes assets

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import NavBar from "./NavBar";
+import MobileNav from "./MobileNav";
+import Link from "next/link";
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <header className="fixed left-0 right-0 w-full bg-white h-[90px] ">
+    <header className="fixed left-0 right-0 w-full bg-primary h-[90px] ">
       <div className=" container mx-auto">
+        <Link href={""}>
+          <Image src={"/assets/img/logo.png"} width={117} height={55} alt="" />
+        </Link>
+        <MobileNav containerStyles="  xl:hidden" />
         <NavBar containerStyles=" flex gap-4 hidden xl:flex" />
       </div>
     </header>
---
+++ app/components/MobileNav.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MobileNav = ({ containerStyles }: { containerStyles: string }) => {
+  return <nav className={`${containerStyles}`}>MobileNav</nav>;
+};
+
+export default MobileNav;
