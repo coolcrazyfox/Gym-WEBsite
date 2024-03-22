a0cf5cb9cfffceb5de0b986c60019cdbf8667007
Andrei Malets <coolviktori@yandex.by>
2024-03-22 11:01:47 +0300

    changes prices

--- app/components/Testimonial.tsx
+++ app/components/Testimonial.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Testimonial = () => {
-  return <section className="h-[50vh] bg-indigo-200">Testimonial</section>;
+  return (
+    <section className="h-[50vh] bg-indigo-200" id="testimonial">
+      Testimonial
+    </section>
+  );
 };
 
 export default Testimonial;
