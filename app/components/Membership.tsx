c9c4bad65a867ee18c2188b45bafcb0b971a8169
Andrei Malets <coolviktori@yandex.by>
2024-03-22 11:01:01 +0300

    changes prices

--- app/components/Membership.tsx
+++ app/components/Membership.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Membership = () => {
-  return <section className="h-screen bg-blue-400">Membership</section>;
+  return (
+    <section className="h-screen bg-blue-400" id="prices">
+      Membership
+    </section>
+  );
 };
 
 export default Membership;
