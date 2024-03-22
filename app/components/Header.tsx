d8a46464249a4ff7fad2d2d2b7467d2fb1734345
Andrei Malets <coolviktori@yandex.by>
2024-03-22 16:16:10 +0300

    changes NavBar

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,9 +3,9 @@ import NavBar from "./NavBar";
 
 const Header = () => {
   return (
-    <header className="fixed left-0 right-0 w-full bg-white ">
+    <header className="fixed left-0 right-0 w-full bg-white h-[90px] ">
       <div className=" container mx-auto">
-        <NavBar containerStyles="bg-red-300 py-12 flex gap-4" />
+        <NavBar containerStyles=" flex gap-4 hidden xl:flex" />
       </div>
     </header>
   );
