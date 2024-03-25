50ae1d4d191ba3dcbefd6e9c60068be959511a0a
Andrei Malets <coolviktori@yandex.by>
2024-03-25 16:22:19 +0300

    changes header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,9 +4,11 @@ import NavBar from "./NavBar";
 import MobileNav from "./MobileNav";
 import Link from "next/link";
 import Image from "next/image";
+import { MdMenu } from "react-icons/md";
 
 const Header = () => {
   const [activeHeader, setActiveHeader] = useState(false);
+  const [activeNavBar, setActiveNavBa] = useState(false);
   useEffect(() => {
     const handleScroll = () => {
       setActiveHeader(window.scrollY > 50);
@@ -27,11 +29,21 @@ const Header = () => {
           <Image src={"/assets/img/logo.png"} width={117} height={55} alt="" />
         </Link>
         <MobileNav
-          containerStyles={`${
-            activeHeader ? "top-[90px]" : "top-[124px] "
+          containerStyles={`${activeHeader ? "top-[90px]" : "top-[124px] "}${
+            activeNavBar
+              ? "max-h-max pt-8 pb-10 border-t border-white/10"
+              : "max-h-0 pt-0 pb-0 overflow-hidden border-white/0"
           } flex flex-col text-center gap-8 fixed bg-primary-200 w-full left-0 text-white text-base uppercase font-medium  transition-all xl:hidden`}
         />
         <NavBar containerStyles=" text-white  uppercase flex gap-4 hidden xl:flex" />
+        <div>
+          <button
+            onClick={() => setActiveNavBa(!activeNavBar)}
+            className="text-white xl:hidden"
+          >
+            <MdMenu className="text-4xl" />
+          </button>
+        </div>
       </div>
     </header>
   );
