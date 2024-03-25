2ca8f5dc6c6c4b727ef12b35eec6ea24965ff8d4
Andrei Malets <coolviktori@yandex.by>
2024-03-25 15:36:40 +0300

    changes Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,18 +1,33 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import NavBar from "./NavBar";
 import MobileNav from "./MobileNav";
 import Link from "next/link";
 import Image from "next/image";
 
 const Header = () => {
+  const [activeHeader, setActiveHeader] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      setActiveHeader(window.scrollY > 50);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <header className="fixed left-0 right-0 w-full bg-primary-200 h-[100px] ">
+    <header
+      className={`${
+        activeHeader ? "h-[100px]" : "h-[124px]"
+      }fixed left-0 right-0 w-full bg-primary-200 h-[100px] `}
+    >
       <div className=" container h-full flex items-center justify-between mx-auto">
         <Link href={""}>
           <Image src={"/assets/img/logo.png"} width={117} height={55} alt="" />
         </Link>
         <MobileNav containerStyles="text-white  xl:hidden" />
-        <NavBar containerStyles=" flex gap-4 hidden xl:flex" />
+        <NavBar containerStyles=" text-white  flex gap-4 hidden xl:flex" />
       </div>
     </header>
   );
