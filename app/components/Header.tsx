67ce0c89be8b0a8c32aa1a43a4b6c6972985ea0b
Andrei Malets <coolviktori@yandex.by>
2024-03-25 15:44:15 +0300

    changes Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -20,13 +20,13 @@ const Header = () => {
     <header
       className={`${
         activeHeader ? "h-[100px]" : "h-[124px]"
-      }fixed left-0 right-0 w-full bg-primary-200 h-[100px] `}
+      }fixed top-0 max-w-[2048px] w-full bg-primary-200 h-[100px] transition-all`}
     >
       <div className=" container h-full flex items-center justify-between mx-auto">
         <Link href={""}>
           <Image src={"/assets/img/logo.png"} width={117} height={55} alt="" />
         </Link>
-        <MobileNav containerStyles="text-white  xl:hidden" />
+        <MobileNav containerStyles="flex flex-col text-white  xl:hidden" />
         <NavBar containerStyles=" text-white  flex gap-4 hidden xl:flex" />
       </div>
     </header>
