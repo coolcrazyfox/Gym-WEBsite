c8c1fc0da17ad602fd8f5738952b2eeca42e69f9
Andrei Malets <coolviktori@yandex.by>
2024-04-06 23:02:18 +0300

    changes About

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -7,7 +7,7 @@ import { fadeIn } from "@/lib/variants";
 const initialState = [
   {
     icon: <FaUsers />,
-    title: "award-winning trainers",
+    title: "personal trainers",
     description:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti laborum vel illo.",
   },
@@ -39,12 +39,15 @@ const About = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 mb-16">
           {initialState.map((item, index) => {
             return (
-              <div key={index}>
-                <div className="text-4xl bg-primary-300 text-white w-[86px] h-[86px] rounded-full flex justify-center items-center">
+              <div
+                key={index}
+                className="flex flex-col justify-center items-center gap-4 border shadow-lg p-10`"
+              >
+                <div className="text-4xl bg-accent text-white w-[80px] h-[80px] rounded-full flex justify-center items-center m-2">
                   {item.icon}
                 </div>
-                <div>
-                  <h4 className="h4 text-accent">{item.title}</h4>
+                <div className="flex flex-col justify-center items-center gap-2 text-center">
+                  <h4 className="h4 text-primary-300 ">{item.title}</h4>
                   <p>{item.description}</p>
                 </div>
               </div>
