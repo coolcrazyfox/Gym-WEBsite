392d9203e9cbdcbbfd54287289a1ae51476bd7c1
Andrei Malets <coolviktori@yandex.by>
2024-04-08 21:42:41 +0300

    changes Classes

--- app/components/Classes.tsx
+++ app/components/Classes.tsx
@@ -3,11 +3,21 @@ import React from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { fadeIn } from "@/lib/variants";
+import SuperButton from "./SuperButton";
+
+const initClasses = [
+  {
+    name: "bodybuilding ",
+    image: "/assets/img/classes/bodybuilding.jpg",
+    description:
+      "Bodybuilding is the practice of progressive resistance exercise to build, control, and develop one's muscles via hypertrophy.",
+  },
+];
 
 const Classes = () => {
   return (
     <section className="h-screen bg-green-300" id="class">
-      Classes
+      <SuperButton text={"sed"} containerStyles="" />
     </section>
   );
 };
