cc7a7c4d6795ce8b089f436cf6c58bfe3cca826a
Andrei Malets <coolviktori@yandex.by>
2024-04-21 17:29:30 +0300

    changes Team

--- app/components/Team.tsx
+++ app/components/Team.tsx
@@ -14,7 +14,7 @@ import SuperButton from "./SuperButton";
 
 const trainerInitData = [
   {
-    avatar: "/assets//img/trainers/david.jpg",
+    avatar: "/assets/img/trainers/nichol.jpg",
     name: "Cameron Nichol",
     role: " CrossFit coach",
     description:
@@ -26,7 +26,7 @@ const trainerInitData = [
     ],
   },
   {
-    avatar: "/assets//img/trainers/rosy.jpg",
+    avatar: "/assets/img/trainers/rosy.jpg",
     name: "Cameron Nichol",
     role: " CrossFit coach",
     description:
@@ -38,9 +38,9 @@ const trainerInitData = [
     ],
   },
   {
-    avatar: "/assets//img/trainers/matt.jpg",
+    avatar: "/assets/img/trainers/david.jpg",
     name: "Cameron Nichol",
-    role: " CrossFit coach",
+    role: " BodyBuilder coach",
     description:
       "Cameron Nichol is recognized for his expertise in rowing as a former Olympic rower and the founder of RowingWOD, where he utilizes his experience to enhance the rowing performance of CrossFit athletes. His approach to integrating rowing with CrossFit is designed to improve overall fitness and athletic ability.",
     social: [
@@ -50,7 +50,7 @@ const trainerInitData = [
     ],
   },
   {
-    avatar: "/assets//img/trainers/sofia.jpg",
+    avatar: "/assets/img/trainers/sofia.jpg",
     name: "Cameron Nichol",
     role: " CrossFit coach",
     description:
@@ -65,8 +65,22 @@ const trainerInitData = [
 
 const Team = () => {
   return (
-    <section className="h-screen bg-pink-300 " id="team">
-      Team
+    <section className="py- xl:h-[110vh] " id="team">
+      <div className="container mx-auto  h-full flex flex-col items-center justify-center">
+        <h2>Our trainers</h2>
+        <div className=" grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-12 mb-12">
+          {trainerInitData.map((trainer, index) => {
+            return (
+              <div key={index}>
+                <div className="relative w-[320px] h-[360px] mx-auto mb-4">
+                  <Image src={trainer.avatar} fill alt="" />
+                </div>
+                {/* <p> {trainer.name}</p> */}
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </section>
   );
 };
