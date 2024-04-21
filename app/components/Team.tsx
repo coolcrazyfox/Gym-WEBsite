ac965faedda15cf222381523c441564a3821154e
Andrei Malets <coolviktori@yandex.by>
2024-04-21 18:54:41 +0300

    changes Team

--- app/components/Team.tsx
+++ app/components/Team.tsx
@@ -27,8 +27,8 @@ const trainerInitData = [
   },
   {
     avatar: "/assets/img/trainers/rosy.jpg",
-    name: "Cameron Nichol",
-    role: " CrossFit coach",
+    name: "Rosy",
+    role: " CardioFitness coach",
     description:
       "Cameron Nichol is recognized for his expertise in rowing as a former Olympic rower and the founder of RowingWOD, where he utilizes his experience to enhance the rowing performance of CrossFit athletes. His approach to integrating rowing with CrossFit is designed to improve overall fitness and athletic ability.",
     social: [
@@ -39,7 +39,7 @@ const trainerInitData = [
   },
   {
     avatar: "/assets/img/trainers/david.jpg",
-    name: "Cameron Nichol",
+    name: "Cameron David",
     role: " BodyBuilder coach",
     description:
       "Cameron Nichol is recognized for his expertise in rowing as a former Olympic rower and the founder of RowingWOD, where he utilizes his experience to enhance the rowing performance of CrossFit athletes. His approach to integrating rowing with CrossFit is designed to improve overall fitness and athletic ability.",
@@ -51,8 +51,8 @@ const trainerInitData = [
   },
   {
     avatar: "/assets/img/trainers/sofia.jpg",
-    name: "Cameron Nichol",
-    role: " CrossFit coach",
+    name: "Cameron Sofia",
+    role: " Fitness coach",
     description:
       "Cameron Nichol is recognized for his expertise in rowing as a former Olympic rower and the founder of RowingWOD, where he utilizes his experience to enhance the rowing performance of CrossFit athletes. His approach to integrating rowing with CrossFit is designed to improve overall fitness and athletic ability.",
     social: [
@@ -82,7 +82,9 @@ const Team = () => {
                 <p className="uppercase text-xs tracking-[3px] mb-2 text-accent">
                   {trainer.role}
                 </p>
-                <p className="mb-6">{trainer.description}</p>
+                <p className="mb-6 max-w-[320px] mx-auto">
+                  {trainer.description}
+                </p>
                 <div className="flex gap-12 justify-center">
                   {trainer.social.map((s, i) => {
                     return (
@@ -100,6 +102,12 @@ const Team = () => {
               </div>
             );
           })}
+        </div>
+        <div>
+          <SuperButton
+            containerStyles="w-[196px] h-[62px]"
+            text="See all trainers"
+          />
         </div>
       </div>
     </section>
