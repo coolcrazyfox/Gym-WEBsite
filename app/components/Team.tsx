9fe5613d48cc81f6aadee040daeb86157cb440d0
Andrei Malets <coolviktori@yandex.by>
2024-04-21 16:55:20 +0300

    changes Hero

--- app/components/Team.tsx
+++ app/components/Team.tsx
@@ -1,4 +1,67 @@
+"use client";
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import { fadeIn } from "@/lib/variants";
+import {
+  FaInstagram,
+  FaFacebookSquare,
+  FaTwitterSquare,
+  FaYoutube,
+} from "react-icons/fa";
+import SuperButton from "./SuperButton";
+
+const trainerInitData = [
+  {
+    avatar: "/assets//img/trainers/david.jpg",
+    name: "Cameron Nichol",
+    role: " CrossFit coach",
+    description:
+      "Cameron Nichol is recognized for his expertise in rowing as a former Olympic rower and the founder of RowingWOD, where he utilizes his experience to enhance the rowing performance of CrossFit athletes. His approach to integrating rowing with CrossFit is designed to improve overall fitness and athletic ability.",
+    social: [
+      { icon: FaInstagram, link: "https://instagram.com" },
+      { icon: FaFacebookSquare, link: "https://facebook.com" },
+      { icon: FaTwitterSquare, link: "https://twitter.com" },
+    ],
+  },
+  {
+    avatar: "/assets//img/trainers/rosy.jpg",
+    name: "Cameron Nichol",
+    role: " CrossFit coach",
+    description:
+      "Cameron Nichol is recognized for his expertise in rowing as a former Olympic rower and the founder of RowingWOD, where he utilizes his experience to enhance the rowing performance of CrossFit athletes. His approach to integrating rowing with CrossFit is designed to improve overall fitness and athletic ability.",
+    social: [
+      { icon: FaInstagram, link: "https://instagram.com" },
+      { icon: FaFacebookSquare, link: "https://facebook.com" },
+      { icon: FaTwitterSquare, link: "https://twitter.com" },
+    ],
+  },
+  {
+    avatar: "/assets//img/trainers/matt.jpg",
+    name: "Cameron Nichol",
+    role: " CrossFit coach",
+    description:
+      "Cameron Nichol is recognized for his expertise in rowing as a former Olympic rower and the founder of RowingWOD, where he utilizes his experience to enhance the rowing performance of CrossFit athletes. His approach to integrating rowing with CrossFit is designed to improve overall fitness and athletic ability.",
+    social: [
+      { icon: FaInstagram, link: "https://instagram.com" },
+      { icon: FaFacebookSquare, link: "https://facebook.com" },
+      { icon: FaTwitterSquare, link: "https://twitter.com" },
+    ],
+  },
+  {
+    avatar: "/assets//img/trainers/sofia.jpg",
+    name: "Cameron Nichol",
+    role: " CrossFit coach",
+    description:
+      "Cameron Nichol is recognized for his expertise in rowing as a former Olympic rower and the founder of RowingWOD, where he utilizes his experience to enhance the rowing performance of CrossFit athletes. His approach to integrating rowing with CrossFit is designed to improve overall fitness and athletic ability.",
+    social: [
+      { icon: FaInstagram, link: "https://instagram.com" },
+      { icon: FaFacebookSquare, link: "https://facebook.com" },
+      { icon: FaTwitterSquare, link: "https://twitter.com" },
+    ],
+  },
+];
 
 const Team = () => {
   return (
