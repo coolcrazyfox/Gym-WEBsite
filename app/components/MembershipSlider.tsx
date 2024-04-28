35823d9a75075e34f5e49c9a7f6a621431fb76c2
Andrei Malets <coolviktori@yandex.by>
2024-04-28 23:16:37 +0300

    changes Membership

--- app/components/MembershipSlider.tsx
+++ app/components/MembershipSlider.tsx
@@ -3,7 +3,9 @@ import React from "react";
 import SuperButton from "./SuperButton";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
-
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
 import { FaClosedCaptioning, FaCheck } from "react-icons/fa6";
 
 const MembershipSlider = () => {
