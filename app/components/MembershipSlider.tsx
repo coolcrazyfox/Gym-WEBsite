5c26b5902113acbdf2f456af4fd9a51a7f9c147e
Andrei Malets <coolviktori@yandex.by>
2024-04-28 23:36:32 +0300

    changes Membership

--- app/components/MembershipSlider.tsx
+++ app/components/MembershipSlider.tsx
@@ -11,15 +11,41 @@ import { FaClosedCaptioning, FaCheck } from "react-icons/fa6";
 const initMembershipData = [
   {
     title: "Standart",
-    price: "30",
+    price: "40",
     benefits: [
-      { icon: FaClosedCaptioning, name: "Includes membership" },
+      { icon: FaCheck, name: "Includes membership" },
       { icon: FaCheck, name: "Access to all GYM facilities" },
       { icon: FaClosedCaptioning, name: "Diet plan included" },
       { icon: FaCheck, name: "health and fitness tips" },
+      { icon: FaClosedCaptioning, name: "Monday -- Friday GYM access " },
+      { icon: FaCheck, name: "Full access to everything" },
+      { icon: FaClosedCaptioning, name: "No additional amenities" },
+    ],
+  },
+  {
+    title: "Premium",
+    price: "100",
+    benefits: [
+      { icon: FaCheck, name: "Includes membership" },
+      { icon: FaCheck, name: "Access to all GYM facilities" },
+      { icon: FaCheck, name: "Diet plan included" },
       { icon: FaCheck, name: "health and fitness tips" },
+      { icon: FaClosedCaptioning, name: "Monday -- Friday GYM access " },
+      { icon: FaCheck, name: "Full access to everything" },
+      { icon: FaClosedCaptioning, name: "No additional amenities" },
+    ],
+  },
+  {
+    title: "Unlimited",
+    price: "250",
+    benefits: [
+      { icon: FaCheck, name: "Includes membership" },
+      { icon: FaCheck, name: "Access to all GYM facilities" },
+      { icon: FaCheck, name: "Diet plan included" },
       { icon: FaCheck, name: "health and fitness tips" },
-      { icon: FaCheck, name: "health and fitness tips" },
+      { icon: FaCheck, name: "Monday -- Friday GYM access " },
+      { icon: FaCheck, name: "Full access to everything" },
+      { icon: FaCheck, name: "No additional amenities" },
     ],
   },
 ];
