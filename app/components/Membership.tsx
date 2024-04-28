b1639f1e76408d2724f1125f7860b3cb227f8a06
Andrei Malets <coolviktori@yandex.by>
2024-04-28 22:55:26 +0300

    changes Membership

--- app/components/Membership.tsx
+++ app/components/Membership.tsx
@@ -2,12 +2,16 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { fadeIn } from "@/lib/variants";
+import MembershipSlider from "./MembershipSlider";
 
 const Membership = () => {
   return (
     <section className="h-screen bg-blue-400" id="prices">
       <div className="container mx-auto">
         <h2>Membership</h2>
+        <div>
+          <MembershipSlider />
+        </div>
       </div>
     </section>
   );
