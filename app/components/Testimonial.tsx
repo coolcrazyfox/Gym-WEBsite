a2aaad0ef9b5b38814b3d44aa19bb63acc761681
Andrei Malets <coolviktori@yandex.by>
2024-05-08 14:59:42 +0300

    changes Testimonial

--- app/components/Testimonial.tsx
+++ app/components/Testimonial.tsx
@@ -1,4 +1,8 @@
 "use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
 import Image from "next/image";
 import { FaQuoteLeft } from "react-icons/fa";
 
