da603350275e895328e979d212e573ce5106a076
Andrei Malets <coolviktori@yandex.by>
2024-05-08 15:24:57 +0300

    changes Feedback

--- app/components/Feedback.tsx
+++ app/components/Feedback.tsx
@@ -6,6 +6,24 @@ import "swiper/css/pagination";
 import Image from "next/image";
 import { FaQuoteLeft } from "react-icons/fa";
 
+const feedbackData = [
+  {
+    avatar: "/assets/img/feedback/lisean.jpg",
+    name: "Lee Sean",
+    comment: "text",
+  },
+  {
+    avatar: "/assets/img/feedback/andrea.jpg",
+    name: "Andrea Cheanotte",
+    comment: "text",
+  },
+  {
+    avatar: "/assets/img/feedback/clark.jpg",
+    name: "Clark Gandert",
+    comment: "text",
+  },
+];
+
 const Feedback = () => {
   return (
     <section className="" id="testimonial">
