a9342b3296118c0890a0489b38cc9850ea661554
Andrei Malets <coolviktori@yandex.by>
2024-05-08 15:30:38 +0300

    changes Feedback

--- app/components/Feedback.tsx
+++ app/components/Feedback.tsx
@@ -4,6 +4,8 @@ import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { fadeIn } from "@/lib/variants";
 import { FaQuoteLeft } from "react-icons/fa";
 
 const feedbackData = [
@@ -26,8 +28,10 @@ const feedbackData = [
 
 const Feedback = () => {
   return (
-    <section className="" id="testimonial">
-      Feedback
+    <section className="py-12 xl:py-28" id="testimonial">
+      <div className="container mx-auto">
+        <h2>Our References</h2>
+      </div>
     </section>
   );
 };
