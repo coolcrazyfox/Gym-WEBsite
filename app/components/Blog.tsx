e25dbd23999f97216da91257e92394953ca555de
Andrei Malets <coolviktori@yandex.by>
2024-05-23 15:40:15 +0300

    changes Blog

--- app/components/Blog.tsx
+++ app/components/Blog.tsx
@@ -7,9 +7,60 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import { fadeIn } from "@/lib/variants";
 
+const initBlogData = [
+  {
+    image: "/assets/img/blog/post1.jpg",
+    date: "February 11, 2024",
+    text: "Maintain a perfect structure after workout",
+    link: "",
+  },
+  {
+    image: "/assets/img/blog/post2.jpg",
+    date: "February 11, 2024",
+    text: "Maintain a perfect structure after workout",
+    link: "",
+  },
+  {
+    image: "/assets/img/blog/post3.jpg",
+    date: "February 11, 2024",
+    text: "Maintain a perfect structure after workout",
+    link: "",
+  },
+  {
+    image: "/assets/img/blog/post4.jpg",
+    date: "February 11, 2024",
+    text: "Maintain a perfect structure after workout",
+    link: "",
+  },
+  {
+    image: "/assets/img/blog/post1.jpg",
+    date: "February 11, 2024",
+    text: "Maintain a perfect structure after workout",
+    link: "",
+  },
+  {
+    image: "/assets/img/blog/post2.jpg",
+    date: "February 11, 2024",
+    text: "Maintain a perfect structure after workout",
+    link: "",
+  },
+  {
+    image: "/assets/img/blog/post3.jpg",
+    date: "February 11, 2024",
+    text: "Maintain a perfect structure after workout",
+    link: "",
+  },
+  {
+    image: "/assets/img/blog/post4.jpg",
+    date: "February 11, 2024",
+    text: "Maintain a perfect structure after workout",
+    link: "",
+  },
+];
+
 const Blog = () => {
   return (
-    <section className="h-screen bg-amber-400" id="blog">
+    <section className=" bg-primary-300 text-white py-24" id="blog">
       Blog
     </section>
   );
