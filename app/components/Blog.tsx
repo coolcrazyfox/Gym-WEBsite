bb6466a6d499ac0c257ebb7041324bd7fffe176c
Andrei Malets <coolviktori@yandex.by>
2024-05-23 15:28:47 +0300

    changes Blog

--- app/components/Blog.tsx
+++ app/components/Blog.tsx
@@ -1,4 +1,11 @@
-import React from "react";
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { fadeIn } from "@/lib/variants";
 
 const Blog = () => {
   return (
