67039869f19a779c96d6b18946ede34bd267ad42
Andrei Malets <coolviktori@yandex.by>
2024-05-25 13:03:04 +0300

    changes Brands

--- app/components/Brands.tsx
+++ app/components/Brands.tsx
@@ -19,20 +19,27 @@ const brandContainerVariant = {
   },
 };
 const brandItem = {
-  hidden: { hidden: { y: 20, opacity: 0 } },
+  hidden: { y: 20, opacity: 0 },
   show: {
     opacity: 1,
-    transition: { duration: 0.5, ease: [0.25, 0.6, 0.3,0.8] },
+    transition: { duration: 0.5, ease: [0.25, 0.6, 0.3, 0.8] },
   },
 };
+
 const Brands = () => {
   return (
     <section className="py-8 " id="contact">
       <div className="container mx-auto  ">
-        <div className=" grid grid-cols-2 lg:grid-cols-5 py-8">
+        <motion.div
+          variants={brandContainerVariant}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+          className=" grid grid-cols-2 lg:grid-cols-5 py-8"
+        >
           {initBrands.map((brand, index) => {
             return (
-              <div key={index}>
+              <motion.div variants={brandItem} key={index}>
                 <Link className="group" href={brand.link}>
                   <Image
                     src={brand.src}
@@ -42,10 +49,10 @@ const Brands = () => {
                     className="opacity-50 group-hover:opacity-100 transition-all mx-auto "
                   />
                 </Link>
-              </div>
+              </motion.div>
             );
           })}
-        </div>
+        </motion.div>
       </div>
     </section>
   );
