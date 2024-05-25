9aca141171b2d02d1e40b23de78cbd4c7d78028a
Andrei Malets <coolviktori@yandex.by>
2024-05-25 11:42:41 +0300

    changes Brands

--- app/components/Brands.tsx
+++ app/components/Brands.tsx
@@ -1,4 +1,15 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+const initBrands = [
+  { src: "/assets/img/brands/brand_1.png", link: "" },
+  { src: "/assets/img/brands/brand_2.png", link: "" },
+  { src: "/assets/img/brands/brand_3.png", link: "" },
+  { src: "/assets/img/brands/brand_4.png", link: "" },
+  { src: "/assets/img/brands/brand_5.png", link: "" },
+];
 
 const Brands = () => {
   return (
